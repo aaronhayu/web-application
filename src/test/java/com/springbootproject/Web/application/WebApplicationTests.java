c0ed2f4ecba9396e1169a213bd8da46ce0a18ed2
Aaron <aaronhayu@gmail.com>
2021-11-18 13:28:40 +0100

    Initial commit

---
+++ src/test/java/com/springbootproject/Web/application/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootproject.Web.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
